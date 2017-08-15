3e90c1f9c3a97af876b029fd20dd761dc047923b
flavio-dev <trash_flavio@hotmail.fr>
2017-08-15 14:34:49 +0100

    fixing the server for heroku

--- server/index.js
+++ server/index.js
@@ -1,101 +1,74 @@
-"use strict";
+const express = require('express');
+const path = require('path');
+const app = express();
+const expressWs = require('express-ws')(app);
 
-// Optional. You will see this name in eg. 'ps' or 'top' command
-process.title = 'chatting';
-
-// Port where we'll run the websocket server
-var webSocketsServerPort = Number(process.env.PORT || 4000);
-
-// websocket and http servers
-var webSocketServer = require('websocket').server;
-var http = require('http');
-var https = require('https');
-
+// Priority serve any static files.
+const PORT = Number(process.env.PORT || 4000);
 // map of user to connection (useful for when deconnection)
 var userConnectionMap = {};
 // list of user names
 var users = [];
 
-/**
- * HTTP server
- */
-var server = http.createServer(function(request, response) {
-  if (request.url === '/users/') {
-    response.writeHead(200, {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type'
-    });
-    response.write(JSON.stringify(users));
-    response.end();
-  }
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+// Answer API requests.
+app.get('/users', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  res.send(JSON.stringify(users));
 });
 
-server.listen(webSocketsServerPort, function() {
-	console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
-});
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msgPassed) {
+    const msgJSON = JSON.parse(msgPassed)
+		console.log((new Date()) + ' Received Parameters: '+ msgPassed);
 
-/**
- * WebSocket server
- */
-var wsServer = new webSocketServer({
-	// WebSocket server is tied to a HTTP server. WebSocket request is just
-	// an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
-	httpServer: server
-});
+    if (msgJSON.username && msgJSON.username.length) {
+      // a username is registering itself
+      // sending to all usrs someone is loggin in
+      users.forEach(user => {
+        const connection = userConnectionMap[user]
+        connection.send(msgPassed);
+      })
 
-// This callback function is called every time someone
-// tries to connect to the WebSocket server
-wsServer.on('request', function(request) {
-	console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-
-	var connection = request.accept(null, request.origin);
-	// we need to know client index to remove them on 'close' event
-
-	console.log((new Date()) + ' Connection accepted.');
-
-	// user sent some message
-	connection.on('message', function(message) {
-		if (message.type === 'utf8') {
-			console.log((new Date()) + ' Received Parameters: '+ message.utf8Data);
-      var msgJSON = JSON.parse(message.utf8Data)
-
-
-      if (msgJSON.username && msgJSON.username.length) {
-        // a username is registering itself
+      userConnectionMap[msgJSON.username] = ws
+      users.push(msgJSON.username)
+    } else {
+      if (msgJSON.to === 'ALL') {
+        // broadcast message to all connected clients
         users.forEach(user => {
-          // sending to all usrs someone is loggin in
           const connection = userConnectionMap[user]
-          connection.sendUTF(message.utf8Data);
+          connection.send(msgPassed);
         })
-        userConnectionMap[msgJSON.username] = connection
-        users.push(msgJSON.username)
       } else {
-        if (msgJSON.to === 'ALL') {
-          // broadcast message to all connected clients
-          users.forEach(user => {
-            const connection = userConnectionMap[user]
-            connection.sendUTF(message.utf8Data);
-          })
-        } else {
-          // broadcast message only to the user, and myself
-          const connectionTo = userConnectionMap[msgJSON.to]
-          connectionTo.sendUTF(message.utf8Data);
-          const connectionFrom = userConnectionMap[msgJSON.from]
-          connectionFrom.sendUTF(message.utf8Data);
-        }
+        // broadcast message only to the user, and myself
+        const connectionTo = userConnectionMap[msgJSON.to]
+        connectionTo.send(msgPassed);
+        const connectionFrom = userConnectionMap[msgJSON.from]
+        connectionFrom.send(msgPassed);
       }
-		}
-	});
+    }
+  });
 
-	// user disconnected
-  // NOT TESTED EITHER =)
-	connection.on('close', function(connection, user) {
-		console.log((new Date()) + " Peer "
+  ws.on('close', function(connection, user) {
+    console.log((new Date()) + " Peer "
 			+ user + " disconnected.");
 		// remove user from the list of connected clients
     userConnectionMap[user] = null
 		users.splice(users.indexOf(user), 1);
-	});
+  });
+
+  console.log('socket', req.testing);
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
 });
