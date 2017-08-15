92f948ac28471ae4de3791c0a63f2da182b72388
flavio-dev <trash_flavio@hotmail.fr>
2017-08-15 01:29:34 +0100

    some tests

---
+++ react-ui/src/components/Layout/Layout.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import Layout from './Layout';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Layout><p>some children</p></Layout>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
