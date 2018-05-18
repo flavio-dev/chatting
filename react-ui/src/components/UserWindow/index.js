35b81498a7ba1f97359fce7a1dbc99147e603d3f
flavio-dev <trash_flavio@hotmail.fr>
2018-05-18 22:06:24 +0200

    initial push

--- react-ui/src/components/UserWindow/index.js
+++
@@ -1,3 +0,0 @@
-import UserWindow from './UserWindow'
-
-export default UserWindow
--- react-ui/src/routes/user/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux-immutable'
-
-import { userIdReducer } from './userId'
-import { userMessagesReducer } from './userMessages'
-
-export default combineReducers({
-  userId: userIdReducer,
-  userMessages: userMessagesReducer
-})
